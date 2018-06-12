394ed815d8577fce25a12f7b16e3f0c4ce2c56ef
yanjingyang@zulong.com <yanjingyang@zulong.com>
2018-06-12 09:21:14 +0800

    first commit

---
+++ src/main/java/com/ascend/concurrency/MoocConcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.ascend.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoocConcurrencyApplication.class, args);
+    }
+}
