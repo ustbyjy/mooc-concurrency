06fc29c4e2cb4974c4096ea1a5548307bceaaac3
yanjingyang@zulong.com <yanjingyang@zulong.com>
2018-06-21 16:16:37 +0800

    Semaphore

---
+++ src/main/java/com/ascend/concurrency/example/aqs/SemaphoreExample3.java
@@ -0,0 +1,42 @@
+package com.ascend.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+public class SemaphoreExample3 {
+
+    private static int threadCount = 200;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        final Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            executorService.execute(() -> {
+                try {
+                    if (semaphore.tryAcquire()) {
+                        test(threadNum);
+                        semaphore.release();
+                    }
+                } catch (InterruptedException e) {
+                    log.info("exception", e);
+                }
+            });
+        }
+
+        executorService.shutdown();
+    }
+
+    private static void test(int threadNum) throws InterruptedException {
+        log.info("{}", threadNum);
+        Thread.sleep(1000);
+    }
+
+}
---
+++ src/main/java/com/ascend/concurrency/example/aqs/SemaphoreExample4.java
@@ -0,0 +1,43 @@
+package com.ascend.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class SemaphoreExample4 {
+
+    private static int threadCount = 200;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        final Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            executorService.execute(() -> {
+                try {
+                    if (semaphore.tryAcquire(5000, TimeUnit.MILLISECONDS)) {
+                        test(threadNum);
+                        semaphore.release();
+                    }
+                } catch (InterruptedException e) {
+                    log.info("exception", e);
+                }
+            });
+        }
+
+        executorService.shutdown();
+    }
+
+    private static void test(int threadNum) throws InterruptedException {
+        log.info("{}", threadNum);
+        Thread.sleep(1000);
+    }
+
+}
