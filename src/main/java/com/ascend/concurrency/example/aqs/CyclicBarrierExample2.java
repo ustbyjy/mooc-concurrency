18e33b7efbe4efb1d72874ef451f0c3503309edc
yanjingyang@zulong.com <yanjingyang@zulong.com>
2018-06-21 16:48:20 +0800

    CyclicBarrier

---
+++ src/main/java/com/ascend/concurrency/example/aqs/CyclicBarrierExample2.java
@@ -0,0 +1,37 @@
+package com.ascend.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+@Slf4j
+public class CyclicBarrierExample2 {
+
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executorService.execute(() -> {
+                try {
+                    race(threadNum);
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+            });
+        }
+    }
+
+    private static void race(int threadNum) throws Exception {
+        Thread.sleep(1000);
+        log.info("{} is ready", threadNum);
+        try {
+            cyclicBarrier.await(2000, TimeUnit.MILLISECONDS);
+        } catch (BrokenBarrierException | TimeoutException e) {
+            log.error("BrokenBarrierException or TimeoutException");
+        }
+        log.info("{} continue", threadNum);
+    }
+}
